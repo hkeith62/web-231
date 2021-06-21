8260a560614d2bbdff897dbc1070bad635cd6695
Keith Hall <hkeith62@gmail.com>
2021-06-20 22:00:37 -0500

    Completed assignment 4- numbers game app.

--- week-3/hello.js
+++ week-3/hello.js
@@ -1,18 +1,12 @@
-<!--
-    Title: WEB 231 – Enterprise JavaScript I
-    Author: Professor Krasso
-    Date: 05/30/2021
-    Modified By: Keith Hall
-    Description: This is the HTML page for web-231 assignment 3, temperature conversion app.
---> 
-<!DOCTYPE html>
+
+ <!DOCTYPE html>
 <html lang="en"> 
 <head>
     <meta charset="UTF-8" /> 
     <title>WEB 231 – Enterprise JavaScript I</title>
     <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
     <meta name="keywords" content="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-    <meta name="robots" content="noindex" />
+    <meta name="robots" content="nofollow" />
     <meta name="author" content="Keith Hall" />
 	<link rel="stylesheet" href="../global.css" />
 	<link rel="stylesheet" href="hall-temp-conversion.css" />
@@ -50,7 +44,11 @@
 function myFunction() {
   var farenheit = document.getElementById("txtFahrenheit").value; // Captures the inputted value of the textfeild with id= txtFahrenheit and assigns it to the variable fahrenheit. 
   document.getElementById("demo").innerHTML = ((parseFloat(farenheit) - 32) / 1.8).toFixed(2); // The inputted value is converted to celcius and fixed to 2 decimal places.
+src="hello.js"
 }
 </script>
 </body>
 </html>
+console.log("hello world");
+ 
+ 
